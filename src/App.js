4985b255f712fa67f6aae4ed7bfbf6d3d07a6862
uv1997dev <166135107+uv1997dev@users.noreply.github.com>
2024-04-11 15:43:26 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MarkersMap from './component/currentLocation';
+
+function App() {
+    return (
+        <div className="App">
+            <MarkersMap />
+        </div>
+    );
+}
+
+export default App;
